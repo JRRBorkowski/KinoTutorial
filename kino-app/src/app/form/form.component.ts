12d5cb916e6b307f653b43f795ecf2304f4e110e
Jakub Borkowski <jakub.borkowski@ppuc.pl>
2022-12-06 16:17:21 +0100

    form validation

--- kino-app/src/app/form/form.component.ts
+++ kino-app/src/app/form/form.component.ts
@@ -1,28 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, NonNullableFormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
+import { AbstractControl, FormGroup, NonNullableFormBuilder, ValidationErrors, ValidatorFn, Validators } from '@angular/forms'
 
+const customValidator = (source: string, target: string) : ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const sourceCtrl = control.get(source)
+    const targetCtrl = control.get(target)
+
+    return sourceCtrl && targetCtrl && sourceCtrl.value !== targetCtrl.value ? { mismatch: true } : null;
+  }
+}
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
 
-  reservationForm = new FormGroup({
-    firstName: new FormControl(''),
-    surname: new FormControl(''),
-    phone: new FormControl('', {
-      validators: Validators.pattern('/ \d{5} /')
+  reservationForm = this.builder.group({
+    firstName: this.builder.control('', {
+      validators: Validators.required
     }),
-    email: new FormControl('', {
-      validators: Validators.email
+    surname: this.builder.control('', {
+      validators: Validators.required
+    }),
+    phone: this.builder.control('', {
+      validators: [
+        // Validators.pattern(/[0-9] /),
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(9)
+      ]
+    }),
+    email: this.builder.control('', {
+        validators: [Validators.required, Validators.email]
+    }),
+    confirmEmail: this.builder.control('', {
+        validators: [Validators.required, Validators.email]
     })
+  }, {
+    validators: customValidator('email', 'confirmEmail')
   })
 
-  constructor(private builder: NonNullableFormBuilder) {
-    this.reservationForm.valueChanges.subscribe(console.log)
+  get reservationCtrl() {
+    return this.reservationForm;
+  }
 
+  constructor(private builder: NonNullableFormBuilder) {}
+
+  emailsMatchValidatorError() {
+    return (
+      this.reservationForm.getError('mismatch') && this.reservationForm.get('confirmEmail')?.touched
+    )
   }
 
   submitForm() {
--- kino-app/src/app/mock-movies.ts
+++ kino-app/src/app/mock-movies.ts
@@ -4,7 +4,7 @@ export const Movies: Movie[] = [
     {   
         id: 1,
         title: "Tucker and Dale VS Evil",
-        description: "Yeehaw pardners",
+        description: "Yeehaw pardners! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         shortDescription: "comedy",
         imageSource: "https://pbs.twimg.com/media/EGAmPkYXkAAfe9q.png",
         viewings: ["12:30", "13:00", "14:00"],
